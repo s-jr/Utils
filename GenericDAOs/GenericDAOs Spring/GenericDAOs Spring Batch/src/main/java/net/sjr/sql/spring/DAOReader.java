d4f5d4f96e564274be717f6506bdb0c61b8f8350
Jan Reichl <jan@team-reichl.de>
2017-07-18 14:33:57 +0200

    Spring Batch Kompatibilität
    closes #12

--- GenericDAOs/GenericDAOs Spring/GenericDAOs Spring Batch/src/main/java/net/sjr/sql/spring/DAOReader.java
+++ GenericDAOs/GenericDAOs Spring/GenericDAOs Spring Batch/src/main/java/net/sjr/sql/spring/DAOReader.java
@@ -1,6 +1,7 @@
 package net.sjr.sql.spring;
 
 import net.sjr.sql.DBObject;
+import net.sjr.sql.ParameterList;
 import org.springframework.batch.item.database.AbstractPagingItemReader;
 
 /**
@@ -8,14 +9,24 @@ import org.springframework.batch.item.database.AbstractPagingItemReader;
  */
 public class DAOReader<T extends DBObject<P>, P extends Number> extends AbstractPagingItemReader<T> {
 	private final PaginationDAO<T, P> dao;
+	private final String join;
+	private final String where;
+	private final ParameterList params;
 
 	public DAOReader(PaginationDAO<T, P> dao) {
+		this(dao, null, null, null);
+	}
+
+	public DAOReader(PaginationDAO<T, P> dao, String join, String where, ParameterList params) {
 		this.dao = dao;
+		this.join = join;
+		this.where = where;
+		this.params = params;
 	}
 
 	@Override
 	protected void doReadPage() {
-		results = dao.loadPage(getPage(), getPageSize());
+		results = dao.loadPage(getPage(), getPageSize(), join, where, params);
 	}
 
 	@Override
